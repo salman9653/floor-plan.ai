a1c4397a1ebf8b7ae2c6aafe8481d4b2a51471a8
salman9653 <salman9915189734@gmail.com>
2026-02-19 19:40:44 +0530

    add hero section on home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +14,47 @@ export default function Home() {
   return (
     <div className="home">
       <Navbar />
-      <h1 className="text-3xl font-extrabold">Welcome to React Router!</h1>
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse" />
+          </div>
+          <p>AI Powered Floor Plan Generator </p>
+        </div>
+        <h1>
+          Build beautiful spaces at the speed of thought with{" "}
+          <span className="text-primary">FloorPlan AI</span>
+        </h1>
+
+        <p className="subtitle">
+          FloorPlan.ai is a AI-first design environment that helps you
+          visualize, render, and ship architectural projects faster than ever.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG formats up to 10mb</p>
+            </div>
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
